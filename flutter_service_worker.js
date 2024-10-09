afe94781844c2b76dbdba9b7eae6d484ef631080
Sithy <sithy@Sithys-MacBook-Pro.local>
2024-10-09 12:44:38 +0700

    New version

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "3be7908a34b02aa329b3ab8625028ae4",
+const RESOURCES = {"flutter_bootstrap.js": "41eed8eff98978c3e953b49298942c0c",
 "version.json": "463271d45e7adcf5bf0193a18a639699",
 "index.html": "5d4f0d663f266e712a163bea0cb3921a",
 "/": "5d4f0d663f266e712a163bea0cb3921a",
 "logosplash.png": "1a6a83d01338c85562c00c084550ba97",
-"main.dart.js": "e2a37ef8fad8982af9cb294c6be6ca99",
+"main.dart.js": "48cb2b34e6efb3a5ebb7aa89924d033c",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "7d7284ae8ba5b4d5c3c56eb3b90953fc",
 "icons/Icon-192.png": "ab3aa79979c5a8f2bd85e20c5f25d9fb",
@@ -17,7 +17,7 @@ const RESOURCES = {"flutter_bootstrap.js": "3be7908a34b02aa329b3ab8625028ae4",
 "icons/Icon-512.png": "57d47017bc0af2f0e2e735614510f924",
 "manifest.json": "5e7315eacda3bc4d107d65781307fd96",
 "assets/AssetManifest.json": "d7b3e0dfd3077642fd308be37cd5c856",
-"assets/NOTICES": "049b13dfddb012cd6440cb236cb2d701",
+"assets/NOTICES": "84b0f3594982b7e1f82515e58fb7ae78",
 "assets/FontManifest.json": "3b1c2e7b2bde77477ad7ba9cd5be43af",
 "assets/AssetManifest.bin.json": "5f4207b2a0264664147857b6b5b29d71",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "e986ebe42ef785b27164c36a9abc7818",
