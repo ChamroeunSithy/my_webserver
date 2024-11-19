29e6c03b019a130e3001aaa7746c97cf6e18d4bf
Sithy <sithy@Sithys-MacBook-Pro.local>
2024-11-19 11:46:29 +0700

    New version

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "e0fd704daea3f6602b179943248876ae",
+const RESOURCES = {"flutter_bootstrap.js": "9b1814d287cf23e5dc4e7a6014db8757",
 "version.json": "463271d45e7adcf5bf0193a18a639699",
 "index.html": "5d4f0d663f266e712a163bea0cb3921a",
 "/": "5d4f0d663f266e712a163bea0cb3921a",
 "logosplash.png": "1a6a83d01338c85562c00c084550ba97",
-"main.dart.js": "6d195f10f1714c6f9ebb8cb448f5f761",
+"main.dart.js": "c0da80636e53e0c45bcd1423738eba6a",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "7d7284ae8ba5b4d5c3c56eb3b90953fc",
 "icons/Icon-192.png": "ab3aa79979c5a8f2bd85e20c5f25d9fb",
