22553b92e923c68cde781ddf6c35b7b1f6b03ff3
Sithy <sithy@Sithys-MacBook-Pro.local>
2024-12-13 12:32:23 +0700

    New version

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2320631378"
+    serviceWorkerVersion: "1298326889"
   }
 });
