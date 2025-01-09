afd0870fe4148acf32f972cde4bddb2bb0fe2498
Sithy <sithy@Sithys-MacBook-Pro.local>
2025-01-09 16:53:55 +0700

    New version

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -6,11 +6,11 @@ Read more: https://developer.mozilla.org/en-US/docs/Web/Security/Secure_Contexts
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"86076592023e3cbd081670f11ce1a59205cd7eda","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
+_flutter.buildConfig = {"engineRevision":"5f77df9269acb57aea28082203b62b7e16d38c29","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
 
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "774427904"
+    serviceWorkerVersion: "3779642721"
   }
 });
